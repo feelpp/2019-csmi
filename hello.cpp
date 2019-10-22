8c13bd809392f96f3ac2e6c83f54b206c89cbf20
JohnBounty <outwawe@gmail.com>
2019-10-22 11:34:17 +0200

    modification de hello: +2 fonctions (fib et carre)

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <stdlib.h>
 
+// Fib
+int f(int n){
+
+  int um1=1, u=1, up1=0;
+  for (int i=2; i<n; i++){
+    up1 = um1 + u;
+    um1 = u;
+    u = up1;
+  }
+  return up1;
+}
+
+// Carre
+int g(int n){
+  return n*n;
+}
+
 
 int main(int argc, char** argv )
 {
   std::cout << "Hello " << getenv("USER") << " !" << std::endl;
-} 
+  // test
+  std::cout << "Fib**2:" <<std::endl;
+  for (int i = 0; i < 10; i++){
+    std::cout << f(i) << " & " << g(f(i)) <<std::endl;
+  }
+
+}
